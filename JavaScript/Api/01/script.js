11bbe247ef44065307cf879aef4d0a9239e6eb0f
Shyam Rana <shyamrana1590@gmail.com>
2026-01-21 16:38:05 +0530

    Delete JavaScript directory

--- JavaScript/Api/01/script.js
+++
@@ -1,4 +0,0 @@
-fetch('https://dog.ceo/api/breeds/image/random')
-.then(response => response.json())
-.then(data => console.log(data))
-.catch(error => console.log("Error :", error));
--- JavaScript/DOM/script.js
+++
@@ -1,2 +0,0 @@
-document.getElementById("heading").innerHTML = ("hi i am leaning DOM friom chai our code")
-document.getElementById("heading").innerText 
